b46c37af9bfc0805ed959da4e03308b6570e6baa
Zayed Akhtar <Zayed.Akhtar@odessainc.com>
2025-11-23 00:52:31 +0530

    Removed connectionstring and added it to .env

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-const config = require('config');
 
-mongoose.connect(`${config.get("MONGODB_URI")}/Text-Summerizer`)
+mongoose.connect(process.env.MONGODB_URI)
 .then(()=>{
     console.log('db connected');
 }).catch(err=> console.log(err));
